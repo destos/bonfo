4e518a4c1d3f5e5ba8c2641d5747978dd4caecdb
Patrick Forringer <patrick@forringer.com>
2022-04-19 21:32:55 -0500

    introduce MSPVersion struct
    Operates as an if else then for the specified MSPVersions enum
    Probably should rename the struct or enum

---
+++ bonfo/msp/versions.py
@@ -0,0 +1,13 @@
+from enum import Enum, IntEnum
+
+from semver import VersionInfo
+
+parse = VersionInfo.parse
+
+
+class MSPVersions(Enum):
+    V1_40 = parse("1.40.0")
+    V1_41 = parse("1.41.0")
+    V1_42 = parse("1.42.0")
+    V1_43 = parse("1.43.0")
+    V1_44 = parse("1.44.0")
