85c6fdf9f1b13b74e86f792b0095eaa4beba963d
Patrick Forringer <patrick@forringer.com>
2022-04-19 21:33:21 -0500

    Create test_structs.py

---
+++ tests/fields/test_structs.py
@@ -0,0 +1,38 @@
+import pytest
+from construct import Default, Int16ub, StreamError
+
+from bonfo.msp.fields.structs import MSPVersion
+from bonfo.msp.versions import MSPVersions
+
+
+def test_mspversion_struct():
+    "When only specifying the msp version, missing data errors, older versions are optional"
+    struct = MSPVersion(Int16ub, MSPVersions.V1_44)
+    found = struct.parse(b"\xff\x12")
+    assert found == 65298
+
+    with pytest.raises(StreamError):
+        struct.parse(b"")
+
+    found = struct.parse(b"", msp=MSPVersions.V1_43)
+    assert found == None
+
+    found = struct.build(None, msp=MSPVersions.V1_43)
+    assert found == b""
+
+
+def test_mspversion_struct_else_subcon():
+    "When providing a non-optional else subcon, it acts differently"
+    struct = MSPVersion(Int16ub, MSPVersions.V1_44, Default(Int16ub, 0))
+    found = struct.parse(b"\xff\x12")
+    assert found == 65298
+
+    with pytest.raises(StreamError):
+        struct.parse(b"")
+
+    with pytest.raises(StreamError):
+        # We error now if nothing is passed for parsing
+        struct.parse(b"", msp=MSPVersions.V1_43)
+
+    found = struct.build(None, msp=MSPVersions.V1_43)
+    assert found == b"\x00\x00"
